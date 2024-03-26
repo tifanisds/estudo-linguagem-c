13a971870ad7b96fd84a678c27fb007a9fbd6c9b
tifanisds <87918491+tifanisds@users.noreply.github.com>
2024-03-26 12:27:01 -0300

    Update exercicio8.c

--- atividade-3/exercicio8.c
+++ atividade-3/exercicio8.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main() {
+    setlocale(LC_ALL, "");
+
+    int numeros[12] = { 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1 };
+    int contador1 = 0;
+    int contador2 = 0;
+    int numerosOrganizados[9];
+
+    while (contador1 < 12){
+      if (numeros[contador1] == 0){
+        numerosOrganizados[contador2++] = numeros[contador1];
+      }
+
+    contador1++;
+    }
+
+    contador1 = 0;
+
+    while (contador1 < 12){
+      if (numeros[contador1] == 1){
+      numerosOrganizados[contador2++] = numeros[contador1];
+      }
+
+    contador1++;
+    }
+
+    contador1 = 0;
+
+    printf("O novo array contem os números ");
+
+    while (contador1 < 12){
+      printf("%d ", numerosOrganizados[contador1]);
+      contador1++;
+    }
+
+    return 0;
+}
