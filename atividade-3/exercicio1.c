949cbe7362d86803330ff75d8fc58619a1879b27
tifanisds <87918491+tifanisds@users.noreply.github.com>
2024-03-26 11:26:20 -0300

    Create exercicio1.c

---
+++ atividade-3/exercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 11;
+
+    if (numero % 2 == 0) {
+        printf("O número %d é par!\n", numero);
+    } else {
+        printf("O número %d é impar!\n", numero);
+    }
+
+    return 0;
+}
